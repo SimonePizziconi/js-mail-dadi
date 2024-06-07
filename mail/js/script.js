7cdc4242f21d715d35eaa2a3902a91156ac999ca
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-07 16:41:56 +0200

    feat: add form in html + add array and variable input in js

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+// Chiedi all’utente la sua email
+let emailUser = document.querySelector("input")
+console.log(emailUser);
+
+// controlla che sia nella lista di chi può accedere,
+const emailList = ["simone@gmail.com", "mario@gmail.com", "marco@gmail.com", "silvia@gmail.com"];
+console.log(emailList);
+
+// stampa un messaggio appropriato sull’esito del controllo.
