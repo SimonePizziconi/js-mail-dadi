6a22d3ffffcaf2de462d879751501a27f0d5530c
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-07 16:51:12 +0200

    feat: add event listener for button and create value for emailUser

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,6 +1,23 @@
-// Chiedi all’utente la sua email
-let emailUser = document.querySelector("input")
+// Prendi l'input da html
+let emailUser = document.querySelector("input");
 console.log(emailUser);
+
+// Prendi il bottone da html
+let button = document.querySelector("button");
+console.log(button);
+
+// chiedi all'utente l'email
+button.addEventListener("click",
+    function (){
+        
+        let emailInput = emailUser.value;
+        console.log(emailInput);
+
+    }
+)
+
+
+
 
 // controlla che sia nella lista di chi può accedere,
 const emailList = ["simone@gmail.com", "mario@gmail.com", "marco@gmail.com", "silvia@gmail.com"];
