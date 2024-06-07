5fbf493f3f8248db5f353614cc08898ba94bae3f
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-07 17:10:33 +0200

    feat: add for, move button to for and send message if email is present in array

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,21 +6,26 @@ console.log(emailUser);
 let button = document.querySelector("button");
 console.log(button);
 
-// chiedi all'utente l'email
-button.addEventListener("click",
-    function (){
-        
-        let emailInput = emailUser.value;
-        console.log(emailInput);
-
-    }
-)
-
-
-
-
-// controlla che sia nella lista di chi può accedere,
+// crea una lista di chi può accedere
 const emailList = ["simone@gmail.com", "mario@gmail.com", "marco@gmail.com", "silvia@gmail.com"];
 console.log(emailList);
 
-// stampa un messaggio appropriato sull’esito del controllo.
+for (let i = 0; i < emailList.length; i++){
+
+    let emailAccess = emailList[i];
+    console.log(emailList[i]);
+
+    button.addEventListener("click",
+        function (){
+
+            // Chiedi all'utente l'email
+            let emailInput = emailUser.value;
+            console.log(emailInput);
+    
+            if (emailInput === emailAccess) {
+                // stampa un messaggio appropriato sull’esito del controllo.
+                console.log("PUOI ACCEDERE");
+            }
+        }
+    )
+}
