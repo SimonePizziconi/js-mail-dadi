c7f085f53e9f0c0a4f5ac5c53db56fe948c53488
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-07 17:36:55 +0200

    feat: show myNumber and pcNumber in console

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+// Genera un numero random da 1 a 6 per te e per il computer
+let myNumber = Math.floor(Math.random() * 6) + 1;
+let pcNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(myNumber, pcNumber);
